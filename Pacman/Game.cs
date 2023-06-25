f6f98bb7fcbf9f5219656121e6335247a15a8c45
Jan Hartman <63947355@cuni.cz>
2023-06-25 21:38:18 +0200

    Move InputManager to separate file

--- Pacman/Game.cs
+++ Pacman/Game.cs
@@ -1,186 +1,6 @@
 ﻿namespace Pacman
 {
     /*
-     * Takes care of all the input data. It is an abstraction so that if I change the way the data is input
-     * I won't have to change the code anywhere else than here. 
-     * 
-     * Also all the program settings can be found here and are not spread across the program.
-     */
-    static class InputManager
-    {
-
-        /*
-         * Sets how different game objects are represented in the map.txt file
-         */
-        private const char blankChar = 'B';
-        private const char wallChar = 'W';
-        private const char heroChar = 'H';
-        private const char pelletChar = 'P';
-        private const char ghostChar = 'G';
-
-        private const int cellSize = 100;
-
-        /*
-         * The sprites are just PNG images converted to a Bitmap to be rendered by the Painter class later.
-         */
-        private static Bitmap blankSprite = Properties.Resources.blank;
-        private static Bitmap wallSprite = Properties.Resources.wall;
-        private static Bitmap heroSprite = Properties.Resources.hero;
-        private static Bitmap pelletSprite = Properties.Resources.pellet;
-        private static Bitmap ghostSprite = Properties.Resources.ghost;
-
-        private static readonly string map = Properties.Resources.map;
-
-        private static StaticGameObject[,] staticGrid;
-        private static DynamicGameObject[,] dynamicGrid;
-        private static List<MovableGameObject> movableGameObjects;
-        private static Hero hero;
-
-        private static bool mapDataLoaded = false;
-        private static bool spriteSizeAdjusted = false;
-
-        public static int GetCellSize()
-        {
-            return cellSize;
-        }
-        public static string GetMap()
-        {
-            return map;
-        }
-        public static Bitmap GetBlankSprite()
-        {
-            if (!spriteSizeAdjusted)
-            {
-                adjustSpriteSize();
-            }
-            return blankSprite;
-        }
-        public static Bitmap GetWallSprite()
-        {
-            if (!spriteSizeAdjusted)
-            {
-                adjustSpriteSize();
-            }
-            return wallSprite;
-        }
-        public static Bitmap GetHeroSprite()
-        {
-            if (!spriteSizeAdjusted)
-            {
-                adjustSpriteSize();
-            }
-            return heroSprite;
-        }
-        public static Bitmap GetPelletSprite()
-        {
-            if (!spriteSizeAdjusted)
-            {
-                adjustSpriteSize();
-            }
-            return pelletSprite;
-        }
-        public static Bitmap GetGhostSprite()
-        {
-            if (!spriteSizeAdjusted)
-            {
-                adjustSpriteSize();
-            }
-            return ghostSprite;
-        }
-        public static Hero GetHero()
-        {
-            return hero;
-        }
-        public static DynamicGameObject[,] GetDynamicGrid()
-        {
-            if (!mapDataLoaded)
-            {
-                prepareMapData();
-                mapDataLoaded = true;
-            }
-            return dynamicGrid;
-        }
-        public static StaticGameObject[,] GetStaticGrid()
-        {
-            if (!mapDataLoaded)
-            {
-                prepareMapData();
-                mapDataLoaded = true;
-            }
-            return staticGrid;
-        }
-        public static List<MovableGameObject> GetMovableGameObjects()
-        {
-            if (!mapDataLoaded)
-            {
-                prepareMapData();
-                mapDataLoaded = true;
-            }
-            return movableGameObjects;
-        }
-        private static void prepareMapData()
-        {
-            string mapString = GetMap();
-            string[] separated = mapString.Split(new[] { "\r\n" }, StringSplitOptions.None);
-
-            int height = separated.Length;
-            int width = separated[0].Length;
-
-            staticGrid = new StaticGameObject[height, width];
-            dynamicGrid = new DynamicGameObject[height, width];
-            movableGameObjects = new List<MovableGameObject>();
-
-            for (int y = 0; y < height; y++)
-            {
-                char[] lineChars = separated[y].ToCharArray();
-                for (int x = 0; x < width; x++)
-                {
-                    char gameObjectChar = lineChars[x];
-                   
-                    switch (gameObjectChar)
-                    {
-                        case blankChar:
-                            staticGrid[y, x] = new StaticBlank();
-                            dynamicGrid[y, x] = new DynamicBlank();
-                            break;
-                        case wallChar:
-                            staticGrid[y, x] = new Wall();
-                            dynamicGrid[y, x] = new DynamicBlank();
-                            break;
-                        case heroChar:
-                            hero = new Hero(x, y);
-                            movableGameObjects.Add(hero);
-                            staticGrid[y, x] = new StaticBlank();
-                            dynamicGrid[y, x] = new DynamicBlank();
-                            break;
-                        case pelletChar:
-                            staticGrid[y, x] = new StaticBlank();
-                            dynamicGrid[y, x] = new Pellet();
-                            break;
-                        case ghostChar:
-                            movableGameObjects.Add(new Ghost(x,y));
-                            staticGrid[y, x] = new StaticBlank();
-                            dynamicGrid[y, x] = new DynamicBlank();
-                            break;
-
-                    }
-                }
-            }
-
-            
-        }
-
-        private static void adjustSpriteSize()
-        {
-            blankSprite = new Bitmap(blankSprite, new Size(cellSize, cellSize));
-            wallSprite = new Bitmap(wallSprite, new Size(cellSize, cellSize));
-            heroSprite = new Bitmap(heroSprite, new Size(cellSize, cellSize));
-            pelletSprite = new Bitmap(pelletSprite, new Size(cellSize, cellSize));
-            ghostSprite = new Bitmap(ghostSprite, new Size(cellSize, cellSize));
-        }
-
-    }
-    /*
      * Takes care of all the game logic. Holds the Map, has access to the Painter and all the variables that 
      * shouldn't be own by some specific object but that should be visible in the entire game as a whole
      */
@@ -288,10 +108,9 @@
 
     }
     /* 
-     * Represents a blank space where nothing lives but in case I wanted to add some behavior I din't want 
-     * it to be null.
+     * Represents a blank space in the static grid. I wanted to be explicit and not relying on null. 
+     * This can be useful for debugging purposes in the future.
      */
-    
     class StaticBlank : StaticGameObject
     {
     
@@ -345,7 +164,8 @@
 
     }
     /*
-     * Represents the underlying grid on which everything moves
+     * Represents the topological space in which everything in the game lives. Has both a grid like map
+     * implemented by two types of grids. Also enables the movable characters to move independently of the grid.
      */
     class Map
     {
@@ -406,6 +226,9 @@
             return movableObjects;
         }
     }
+    /*
+     * Takes care of drawing to the form. Implements buffering to get rid of the flickering.
+     */
     class Painter
     {
         private Graphics formGraphics;
---
+++ Pacman/InputManager.cs
@@ -0,0 +1,183 @@
+﻿namespace Pacman
+{
+    /*
+     * Takes care of all the input data. It is an abstraction so that if I change the way the data is input
+     * I won't have to change the code anywhere else than here. 
+     * 
+     * Also all the program settings can be found here and are not spread across the program.
+     */
+    static class InputManager
+    {
+
+        /*
+         * Sets how different game objects are represented in the map.txt file
+         */
+        private const char blankChar = 'B';
+        private const char wallChar = 'W';
+        private const char heroChar = 'H';
+        private const char pelletChar = 'P';
+        private const char ghostChar = 'G';
+
+        private const int cellSize = 100;
+
+        /*
+         * The sprites are just PNG images converted to a Bitmap to be rendered by the Painter class later.
+         */
+        private static Bitmap blankSprite = Properties.Resources.blank;
+        private static Bitmap wallSprite = Properties.Resources.wall;
+        private static Bitmap heroSprite = Properties.Resources.hero;
+        private static Bitmap pelletSprite = Properties.Resources.pellet;
+        private static Bitmap ghostSprite = Properties.Resources.ghost;
+
+        private static readonly string map = Properties.Resources.map;
+
+        private static StaticGameObject[,] staticGrid;
+        private static DynamicGameObject[,] dynamicGrid;
+        private static List<MovableGameObject> movableGameObjects;
+        private static Hero hero;
+
+        private static bool mapDataLoaded = false;
+        private static bool spriteSizeAdjusted = false;
+
+        public static int GetCellSize()
+        {
+            return cellSize;
+        }
+        public static string GetMap()
+        {
+            return map;
+        }
+        public static Bitmap GetBlankSprite()
+        {
+            if (!spriteSizeAdjusted)
+            {
+                adjustSpriteSize();
+            }
+            return blankSprite;
+        }
+        public static Bitmap GetWallSprite()
+        {
+            if (!spriteSizeAdjusted)
+            {
+                adjustSpriteSize();
+            }
+            return wallSprite;
+        }
+        public static Bitmap GetHeroSprite()
+        {
+            if (!spriteSizeAdjusted)
+            {
+                adjustSpriteSize();
+            }
+            return heroSprite;
+        }
+        public static Bitmap GetPelletSprite()
+        {
+            if (!spriteSizeAdjusted)
+            {
+                adjustSpriteSize();
+            }
+            return pelletSprite;
+        }
+        public static Bitmap GetGhostSprite()
+        {
+            if (!spriteSizeAdjusted)
+            {
+                adjustSpriteSize();
+            }
+            return ghostSprite;
+        }
+        public static Hero GetHero()
+        {
+            return hero;
+        }
+        public static DynamicGameObject[,] GetDynamicGrid()
+        {
+            if (!mapDataLoaded)
+            {
+                prepareMapData();
+                mapDataLoaded = true;
+            }
+            return dynamicGrid;
+        }
+        public static StaticGameObject[,] GetStaticGrid()
+        {
+            if (!mapDataLoaded)
+            {
+                prepareMapData();
+                mapDataLoaded = true;
+            }
+            return staticGrid;
+        }
+        public static List<MovableGameObject> GetMovableGameObjects()
+        {
+            if (!mapDataLoaded)
+            {
+                prepareMapData();
+                mapDataLoaded = true;
+            }
+            return movableGameObjects;
+        }
+        private static void prepareMapData()
+        {
+            string mapString = GetMap();
+            string[] separated = mapString.Split(new[] { "\r\n" }, StringSplitOptions.None);
+
+            int height = separated.Length;
+            int width = separated[0].Length;
+
+            staticGrid = new StaticGameObject[height, width];
+            dynamicGrid = new DynamicGameObject[height, width];
+            movableGameObjects = new List<MovableGameObject>();
+
+            for (int y = 0; y < height; y++)
+            {
+                char[] lineChars = separated[y].ToCharArray();
+                for (int x = 0; x < width; x++)
+                {
+                    char gameObjectChar = lineChars[x];
+
+                    switch (gameObjectChar)
+                    {
+                        case blankChar:
+                            staticGrid[y, x] = new StaticBlank();
+                            dynamicGrid[y, x] = new DynamicBlank();
+                            break;
+                        case wallChar:
+                            staticGrid[y, x] = new Wall();
+                            dynamicGrid[y, x] = new DynamicBlank();
+                            break;
+                        case heroChar:
+                            hero = new Hero(x, y);
+                            movableGameObjects.Add(hero);
+                            staticGrid[y, x] = new StaticBlank();
+                            dynamicGrid[y, x] = new DynamicBlank();
+                            break;
+                        case pelletChar:
+                            staticGrid[y, x] = new StaticBlank();
+                            dynamicGrid[y, x] = new Pellet();
+                            break;
+                        case ghostChar:
+                            movableGameObjects.Add(new Ghost(x, y));
+                            staticGrid[y, x] = new StaticBlank();
+                            dynamicGrid[y, x] = new DynamicBlank();
+                            break;
+
+                    }
+                }
+            }
+
+
+        }
+
+        private static void adjustSpriteSize()
+        {
+            blankSprite = new Bitmap(blankSprite, new Size(cellSize, cellSize));
+            wallSprite = new Bitmap(wallSprite, new Size(cellSize, cellSize));
+            heroSprite = new Bitmap(heroSprite, new Size(cellSize, cellSize));
+            pelletSprite = new Bitmap(pelletSprite, new Size(cellSize, cellSize));
+            ghostSprite = new Bitmap(ghostSprite, new Size(cellSize, cellSize));
+        }
+
+    }
+}
