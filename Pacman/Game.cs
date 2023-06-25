bdf7267ca322e21ea5ab9a5a02abeea601f0e57f
Jan Hartman <63947355@cuni.cz>
2023-06-25 14:04:28 +0200

    Add comments

--- Pacman/Game.cs
+++ Pacman/Game.cs
@@ -9,15 +9,30 @@ using System.Threading.Tasks;
 
 namespace Pacman
 {
+    /*
+     * Takes care of all the input data. It is an abstraction so that if I change the way the data is input
+     * I won't have to change the code anywhere else than here. 
+     * 
+     * Also all the program settings can be found here and are not spread across the program.
+     */
     static class InputManager
     {
 
+        /*
+         * The sprites are just PNG images converted to a Bitmap to be rendered by the Painter class later.
+         */
         private static readonly Bitmap blankSprite = Properties.Resources.blank;
         private static readonly Bitmap wallSprite = Properties.Resources.wall;
         private static readonly Bitmap heroSprite = Properties.Resources.hero;
         private static readonly Bitmap pelletSprite = Properties.Resources.pellet;
         private static readonly Bitmap ghostSprite = Properties.Resources.ghost;
-         
+
+        private static readonly char blankChar = 'B';
+        private static readonly char wallChar = 'W';
+        private static readonly char heroChar = 'H';
+        private static readonly char pelletChar = 'P';
+        private static readonly char ghostChar = 'G';
+
         private static readonly int size = 100;
         
         private static readonly string map = Properties.Resources.map;
@@ -26,7 +41,7 @@ namespace Pacman
         {
             return size;
         }
-        public static string GetMap()
+        private static string GetMap()
         {
             return map;
         }
@@ -38,24 +53,63 @@ namespace Pacman
         {
             return wallSprite;
         }
-
         public static Bitmap GetHeroSprite()
         {
             return heroSprite;
         }
-
         public static Bitmap GetPelletSprite()
         {
             return pelletSprite;
         }
-
         public static Bitmap GetGhostSprite()
         {
             return ghostSprite;
         }
+        public static GameObject[,] GetGrid()
+        {
+            string mapString = GetMap();
+            string[] separated = mapString.Split(new[] { "\r\n" }, StringSplitOptions.None);
 
+            int height = separated.Length;
+            int width = separated[0].Length;
+
+            GameObject[,] grid = new GameObject[height, width];
+
+            for (int y = 0; y < height; y++)
+            {
+                char[] lineChars = separated[y].ToCharArray();
+                for (int x = 0; x < width; x++)
+                {
+                    char gameObjectChar = lineChars[x];
+                    //Console.WriteLine($"({y},{x}) {gameObjectChar}");
+                    switch (gameObjectChar)
+                    {
+                        case 'B':
+                            grid[y, x] = new Blank();
+                            break;
+                        case 'W':
+                            grid[y, x] = new Wall();
+                            break;
+                        case 'H':
+                            grid[y, x] = new Hero();
+                            break;
+                        case 'P':
+                            grid[y, x] = new Pellet();
+                            break;
+                        case 'G':
+                            grid[y, x] = new Ghost();
+                            break;
+
+                    }
+                }
+            }
+        }
 
     }
+    /*
+     * Takes care of all the game logic. Holds the Map, has access to the Painter and all the variables that 
+     * shouldn't be own by some specific object but that should be visible in the entire game as a whole
+     */
     class GameManager
     {
         private Map map;
@@ -75,6 +129,9 @@ namespace Pacman
         }
     }
 
+    /*
+     * Represents a 2D integer vector
+     */
     struct Direction
     {
         public int X { get; set; }
@@ -86,10 +143,19 @@ namespace Pacman
             Y = y;
         }
     }
+
+    /*
+     * Everything that lives inside the game should inherit this. It has to be something that can 
+     * occupy a grid and that can be drawn
+     */
     abstract class GameObject
     {
        
     }
+
+    /* 
+     * Special type of GameObject that can also move around
+     */
     abstract class MovableGameObject : GameObject
     {
         protected abstract int xPos { get; set; }
@@ -98,14 +164,28 @@ namespace Pacman
         public abstract void Move(Map map);
         public abstract void SetDirection(int dx, int dy);
     }
+
+    /* 
+     * Represents a blank space where nothing lives but in case I wanted to add some behavior I din't want 
+     * it to be null.
+     */
     class Blank : GameObject
     {
-
+    
     }
+
+    /* 
+     * A wall that the player will collide with.
+     */
     class Wall : GameObject
     {
 
     }
+
+    /*
+     * Main playable character of the game. So far I will make it non playable but will
+     * add controls later.
+     */
     class Hero : MovableGameObject
     {
 
@@ -144,16 +224,27 @@ namespace Pacman
         }
 
     }
+
+    /*
+     * Things that player eats and gets points for that
+     */
     class Pellet : GameObject
     {
 
     }
+
+    /* 
+     * Enemies that will be chasing the player
+     */
     class Ghost : GameObject
     {
 
     }
     
     //TODO: implement movement of a player, communication with Map
+    /*
+     * Represents the underlying grid on which everything moves
+     */
     class Map
     {
 
@@ -167,44 +258,10 @@ namespace Pacman
             readMap();
         }
 
+        
         private void readMap()
         {
-            string mapString = InputManager.GetMap();
-            string[] separated = mapString.Split(new[] { "\r\n" },StringSplitOptions.None);
-
-            height = separated.Length;
-            width = separated[0].Length;
-
-            grid = new GameObject[height, width];
-
-            for (int y = 0; y < height; y++)
-            {
-                char[] lineChars = separated[y].ToCharArray();
-                for (int x = 0; x < width; x++)
-                {
-                    char gameObjectChar = lineChars[x];
-                    //Console.WriteLine($"({y},{x}) {gameObjectChar}");
-                    switch (gameObjectChar)
-                    {
-                        case 'B':
-                            grid[y, x] = new Blank();
-                            break;
-                        case 'W':
-                            grid[y, x] = new Wall();
-                            break;
-                        case 'H':
-                            grid[y, x] = new Hero();
-                            break;
-                        case 'P':
-                            grid[y, x] = new Pellet();
-                            break;
-                        case 'G':
-                            grid[y, x] = new Ghost();
-                            break;
-
-                    }
-                }
-            }
+           
 
         }
 
@@ -288,6 +345,9 @@ namespace Pacman
             }
         }
 
+        /*
+         * Initializes all the bitmaps for the corresponding GameObjects from the InputManager
+         */
         private void loadSprites()
         {
             blankSprite = new Bitmap(InputManager.GetBlankSprite(), new Size(spriteSize,spriteSize));
